4736df98f83d7ad712eb84cf01f98016b882ab71
stase <stasenko-andrei2002@mail.ru>
2022-04-17 22:25:23 +0300

    created all directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchUsersComponent } from './search-users/search-users.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { CardUserComponent } from './card-user/card-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchUsersComponent,
+    ListUsersComponent,
+    CardUserComponent
   ],
   imports: [
     BrowserModule
